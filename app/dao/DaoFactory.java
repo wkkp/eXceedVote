21425a1ad4da1a86a52300b94c4f69d53f4d73c8
Piyanggoon Pavachansatit <pii3eiir@hotmail.com>
2012-11-29 11:27:51 +0700

    create new dao class

---
+++ app/dao/DaoFactory.java
@@ -0,0 +1,49 @@
+public class DaoFactory
+{
+	private static DaoFactory df = null;
+
+	private UserDao user;
+	private BallotDao ballot;
+	private CriteriaDao criteria;
+	private ProjectDao project;
+
+	private DaoFactory()
+	{
+		this.criteria = new CriteriaDao();
+		this.project = new ProjectDao();
+		this.user = new UserDao();
+		this.ballot = new BallotDao();
+	}
+
+	public static DaoFactory getInstance()
+	{
+		if (df == null) 
+		{
+			df = new DaoFactory();
+			
+		}
+
+		return df;
+	}
+
+	public UserDao getDao()
+	{
+		return this.user;
+	}
+
+	public BallotDao getBallot()
+	{
+		return this.ballot;
+	}
+
+	public CriteriaDao getCriteria()
+	{
+		return this.criteria;
+	}
+
+	public ProjectDao getProject()
+	{
+		return this.project;
+	}
+
+}
