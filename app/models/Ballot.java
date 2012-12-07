92d91c2efbd45f0ef4a58c8a2b59c2c17c30cec3
Kanisorn Wirutkul <qrukna2@hotmail.com>
2012-12-08 00:27:47 +0700

    fix conflict

--- app/models/Ballot.java
+++ app/models/Ballot.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package models;
 
 import java.util.*;
@@ -33,42 +32,4 @@ public class Ballot extends Model {
 			ballot.save();
 		}
 	}
-
-=======
-package models;
-
-import java.util.*;
-import javax.persistence.*;
-
-import play.db.ebean.*;
-import play.data.format.*;
-import play.data.validation.*;
-
-@Entity
-@Table(name="ballot")
-public class Ballot extends Model {
-
-	@Id
-	public Long bid;
-	public Long user_id;
-	public Long project_id;
-	public Long criteria_id;
-	public int score;
-	private static final long serialVersionUID = 1L;
-
-	public static Finder<Long,Ballot> find = new Finder(Long.class, Ballot.class);
-
-	public Ballot() {
-		super();	
-	}
-
-	public static void saveBallot(Ballot ballot, User user) {
-		if (user.ballotqnty >= ballot.score) {
-			user.ballotqnty -= ballot.score;
-			user.update();
-			ballot.save();
-		}
-	}
-
->>>>>>> a88f87e7cab8d3dff54fa2625bb64bdfcc92905d
 }
