2b42b8f1df707eee23d391efb591a4b9bf09d5d8
gabfest <a_tom007@hotmail.com>
2012-12-16 18:15:21 +0700

    edit text in countdown

--- public/stylesheets/jquery.countdown.js
+++ public/stylesheets/jquery.countdown.js
@@ -17,9 +17,9 @@ function Countdown() {
 	this.regional = []; // Available regional settings, indexed by language code
 	this.regional[''] = { // Default regional settings
 		// The display texts for the counters
-		labels: ['Years', 'Months', 'Weeks', 'Days', 'Hours', 'Minutes', 'Seconds'],
+		labels: ['years', 'months', 'weeks', 'days', 'hrs', 'mins', 'secs'],
 		// The display texts for the counters if only one
-		labels1: ['Year', 'Month', 'Week', 'Day', 'Hour', 'Minute', 'Second'],
+		labels1: ['year', 'month', 'week', 'day', 'hr', 'min', 'sec'],
 		compactLabels: ['y', 'm', 'w', 'd'], // The compact texts for the counters
 		whichLabels: null, // Function to determine which labels to use
 		digits: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], // The digits to display
